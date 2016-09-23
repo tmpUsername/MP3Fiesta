052344a1c88d2daa0a0fb243f120e6bcd31c5386
tmpUsername <admin@fddf-PC>
2016-09-23 14:32:38 +0200

    ajout de fichier

---
+++ test/mp3fiesta/entity/test/MP3FiestTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mp3fiesta.entity.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author admin
+ */
+public class MP3FiestTest {
+    
+    public MP3FiestTest() {
+    }
+    
+}
